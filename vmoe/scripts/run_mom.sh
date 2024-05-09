60bffe9873bd43bd1e6bd266aef2aa57daac439c
rachtsy <41174077+rachtsy@users.noreply.github.com>
2024-05-09 17:01:28 +0800

    Update run_mom.sh

--- vmoe/scripts/run_mom.sh
+++ vmoe/scripts/run_mom.sh
@@ -4,7 +4,7 @@ for i in 1
     then 
         CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python /root/repos/vmoe/main.py \
 			--config /root/repos/vmoe/vmoe/configs/vmoe_paper/vmoe_s32_last2_ilsvrc2012_randaug_light1.py \
-			--workdir /root/checkpoints/vmoe_mom_LN_08/ --jobname vmoe_mom_LN_08
+			--workdir /root/checkpoints/vmoe_mom_LN_09/ --jobname vmoe_mom_LN_09
     else
 		CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python /root/repos/vmoe_mom/main.py \
 			--config /root/repos/vmoe_mom/vmoe/configs/vmoe_paper/vmoe_s32_last2_ilsvrc2012_randaug_light1.py \
